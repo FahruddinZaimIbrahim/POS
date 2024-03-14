60794833e4abb11e6c4eb2ea0445c521406f9a43
fahrudinibrahim <116633912+FahruddinZaimIbrahim@users.noreply.github.com>
2024-03-14 21:44:52 +0700

    Jobsheet4

---
+++ resources/views/user_count.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<head>
+    <title>User Data</title>
+</head>
+<body>
+    <h1>Data User</h1>
+    <table border="1" cellpadding="2" cellspacing="0">
+    <tr>
+        <th>Jumlah Pengguna</th>
+    </tr>
+    <tr>
+        <td>{{$userCount}}</td>
+    </tr>
+    </table>
+</body>
+</html>
